70f05863e2ca08e4011b36fc7acfa70df90d7436
josh.arrowsmith@hotmail.com <josh.arrowsmith@hotmail.com>
2022-05-09 23:54:11 +1000

    env

--- shopify.config.js
+++ shopify.config.js
@@ -1,5 +1,5 @@
 export default {
-  storeDomain: 'hydrogen-preview.myshopify.com',
-  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storeDomain: process.env.SITE_URL,
+  storefrontToken: process.env.TOKEN,
   storefrontApiVersion: '2022-04',
 };
