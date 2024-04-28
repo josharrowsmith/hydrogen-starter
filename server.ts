b3ece3d38e8b3523abd83dd477a6295c55be812a
Josh Arrowsmith <josh.arrowsmith@hotmail.com>
2024-04-29 02:17:11 +1000

    a start

--- server.ts
+++ server.ts
@@ -16,8 +16,8 @@ import {
   type Session,
 } from '@netlify/remix-runtime';
 
-import {createRequestHandler} from '@netlify/remix-edge-adapter';
-import type {Context} from '@netlify/edge-functions';
+import { createRequestHandler } from '@netlify/remix-edge-adapter';
+import type { Context } from '@netlify/edge-functions';
 
 export default async function handler(
   request: Request,
@@ -41,8 +41,8 @@ export default async function handler(
     /**
      * Create Hydrogen's Storefront client.
      */
-    const {storefront} = createStorefrontClient({
-      i18n: {language: 'EN', country: 'US'},
+    const { storefront } = createStorefrontClient({
+      i18n: { language: 'EN', country: 'US' },
       publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
       privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
       storeDomain: env.PUBLIC_STORE_DOMAIN ?? 'mock.shop',
@@ -50,22 +50,11 @@ export default async function handler(
       storefrontHeaders: getStorefrontHeaders(request, context),
     });
 
-    /*
-     * Create a cart handler that will be used to
-     * create and update the cart in the session.
-     */
-    const cart = createCartHandler({
-      storefront,
-      getCartId: cartGetIdDefault(request.headers),
-      setCartId: cartSetIdDefault(),
-      cartQueryFragment: CART_QUERY_FRAGMENT,
-    });
 
     const loadContext = {
       ...context,
       session,
       storefront,
-      cart,
       env,
     };
 
@@ -91,14 +80,14 @@ export default async function handler(
        * If the redirect doesn't exist, then `storefrontRedirect`
        * will pass through the 404 response.
        */
-      return storefrontRedirect({request, response, storefront});
+      return storefrontRedirect({ request, response, storefront });
     }
 
     return response;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
-    return new Response('An unexpected error occurred', {status: 500});
+    return new Response('An unexpected error occurred', { status: 500 });
   }
 }
 
@@ -180,109 +169,6 @@ export class HydrogenSession {
     return this.#sessionStorage.commitSession(this.#session);
   }
 }
-
-// NOTE: https://shopify.dev/docs/api/storefront/latest/queries/cart
-const CART_QUERY_FRAGMENT = `#graphql
-  fragment Money on MoneyV2 {
-    currencyCode
-    amount
-  }
-  fragment CartLine on CartLine {
-    id
-    quantity
-    attributes {
-      key
-      value
-    }
-    cost {
-      totalAmount {
-        ...Money
-      }
-      amountPerQuantity {
-        ...Money
-      }
-      compareAtAmountPerQuantity {
-        ...Money
-      }
-    }
-    merchandise {
-      ... on ProductVariant {
-        id
-        availableForSale
-        compareAtPrice {
-          ...Money
-        }
-        price {
-          ...Money
-        }
-        requiresShipping
-        title
-        image {
-          id
-          url
-          altText
-          width
-          height
-
-        }
-        product {
-          handle
-          title
-          id
-        }
-        selectedOptions {
-          name
-          value
-        }
-      }
-    }
-  }
-  fragment CartApiQuery on Cart {
-    id
-    checkoutUrl
-    totalQuantity
-    buyerIdentity {
-      countryCode
-      customer {
-        id
-        email
-        firstName
-        lastName
-        displayName
-      }
-      email
-      phone
-    }
-    lines(first: $numCartLines) {
-      nodes {
-        ...CartLine
-      }
-    }
-    cost {
-      subtotalAmount {
-        ...Money
-      }
-      totalAmount {
-        ...Money
-      }
-      totalDutyAmount {
-        ...Money
-      }
-      totalTaxAmount {
-        ...Money
-      }
-    }
-    note
-    attributes {
-      key
-      value
-    }
-    discountCodes {
-      code
-      applicable
-    }
-  }
-` as const;
 
 if (process.env.NODE_ENV === 'development') {
   // Tell remix dev that the server is ready
